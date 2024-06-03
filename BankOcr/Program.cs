e6a58bb5248fc709b3a2f062d0cd61d8b3265e1e
mkowalski <mkowalski@future-processing.com>
2024-06-04 01:40:09 +0200

    v3

--- BankOcr/Program.cs
+++ BankOcr/Program.cs
@@ -12,10 +12,5 @@ List<OcrBankAccount> ocrBankAccounts = ocrBankAccountsReader.Read(fileLines);
 BankAccountParser bankAccountParser = new BankAccountParser();
 List<BankAccount> bankAccounts = bankAccountParser.ParseFromOcr(ocrBankAccounts);
 
-Console.WriteLine("Valid");
-foreach (var bankAccount in bankAccounts.Where(a => a.ValidChecksum == true))
-    Console.WriteLine(bankAccount.AccountNumber);
-
-Console.WriteLine("Invalid");
-foreach (var bankAccount in bankAccounts.Where(a => a.ValidChecksum == false))
-    Console.WriteLine(bankAccount.AccountNumber);
+foreach (BankAccount bankAccount in bankAccounts)
+    Console.WriteLine(bankAccount.ToString());
